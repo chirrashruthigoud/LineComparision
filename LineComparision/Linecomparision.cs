73153f25f5cff95519abac8c36e93583890c6b22
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-24 08:50:56 +0530

    Add : calculating the length of line

---
+++ LineComparision/Linecomparision.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class Linecomparision
+    {
+        int x_One, x_Three,y_One, y_Two;
+        //constructor
+        public Linecomparision(int x_One,int x_Two,int y_One,int y_Two)
+        { 
+            this.x_One = x_One;
+            x_Three = x_Two;
+            this.y_One = y_One;
+            this.y_Two = y_Two;
+        
+        }
+        //method
+        public void Calculate()
+        {
+            double length=Math.Sqrt(Math.Pow((x_Three-x_One),2)+Math.Pow((y_Two-y_One),2));
+            Console.WriteLine($"Length of line : { length}");
+
+        }
+
+    }
+}
---
+++ LineComparision/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LineComparision
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Linecomparision cal = new Linecomparision(20,30,50,60);
+            cal.Calculate();
+            Console.ReadKey();
+
+        }
+    }
+}
